a1383e27e03fbae8a68ed6e140961961584ef158
Eriza Fazli <erizzaaaaa@gmail.com>
2015-02-14 12:56:03 +0100

    parent - child works

--- docxreq.py
+++ docxreq.py
@@ -4,6 +4,7 @@ except ImportError:
     from xml.etree.ElementTree import XML
 import zipfile
 import doorstop
+from doorstop.common import DoorstopError
 
 """
 Extract requirements from semi-structured MS Word (.docx) document
@@ -11,76 +12,88 @@ Extract requirements from semi-structured MS Word (.docx) document
 WORD_NAMESPACE = '{http://schemas.openxmlformats.org/wordprocessingml/2006/main}'
 PARA = WORD_NAMESPACE + 'p'
 
-class Document(object):
-    def __init__(self, docpath, repopath):
-        document = zipfile.ZipFile(docpath)
-        xml_content = document.read('word/document.xml')
-        document.close()
-        self.doctree = XML(xml_content)
-        self.repopath = repopath
-        self.tree = doorstop.build(root=repopath)
+def _read_next(parg_iterator):
+    next_parg = next(parg_iterator)
+    return ''.join([x for x in next_parg.itertext()])
 
-    def _read_next(self, parg_iterator):
-        next_parg = next(parg_iterator)
-        return ''.join([x for x in next_parg.itertext()])
+def _read_next_and_forward(parg_iterator):
+    text = _read_next(parg_iterator)
+    next(parg_iterator)
+    return text
 
-    def _read_next_and_forward(self, parg_iterator):
-        text = self._read_next(parg_iterator)
-        next(parg_iterator)
-        return text
+def _create(tree, path, value, parent):
+    try:
+        doc = tree.create_document(path=path, value=value, parent=parent)
+    except DoorstopError as e:
+        raise e
+    else:
+        return doc
 
-    def sync_requirements(self):
-        paragraph = self.doctree.getiterator(PARA)
-        input_reqs = []
+def _find(tree, path, value, parent):
+    try:
+        doc = tree.find_document(value)
+    except DoorstopError as e:
+        raise e
+    else:
+        return doc
 
-        while True:
-            try:
-                text = self._read_next(paragraph)
-                if not text:
-                    continue
 
-                if text == 'REQ_TYPE':
-                    prefix = self._read_next_and_forward(paragraph)
-                    parent = self._read_next(paragraph)
-                    treepath = [self.repopath] if parent == '' else [self.repopath, parent]
+def process_document(repopath, tree, doctree, docfun=_create):
+    paragraph = doctree.getiterator(PARA)
+    input_reqs = []
+    reqs = {}
 
-                    try:
-                        doc = self.tree.create_document(path='/'.join(treepath),
-                                                        value=prefix,
-                                                        parent=parent)
-                    except doorstop.common.DoorstopError:
-                        doc = self.tree.find_document(prefix)
+    while True:
+        try:
+            text = _read_next(paragraph)
+            if not text:
+                continue
 
-                    reqs = dict(zip([x.number for x in doc.items], 
-                                    [x.uid for x in doc.items]))
+            if text == 'REQ_TYPE':
+                prefix = _read_next_and_forward(paragraph)
+                parent = _read_next(paragraph)
+                treepath = '/'.join([repopath, prefix.lower()])
 
-                elif text == 'REQ_NUM':
-                    for key in ['REQ_NUM', 'REQ_TEXT', 'REQ_RATIO', 'REQ_NOTE']:
-                        field = self._read_next_and_forward(paragraph)
+                try:
+                    doc = docfun(tree,
+                                 path=treepath,
+                                 value=prefix,
+                                 parent=parent)
+                except Exception as e:
+                    print(str(e))
+                    break
 
-                        if key == 'REQ_NUM':
-                            num = int(field)
-                            if num not in reqs:
-                                # create
-                                item = doc.add_item(num)
-                                print('new requirement')
-                            else:
-                                # update
-                                uid = prefix + '{:0>3d}'.format(num)
-                                item = doc.find_item(uid)
-                                print('update requirement')
-                            input_reqs.append(num)
+                reqs = dict(zip([x.number for x in doc.items], 
+                                [x.uid for x in doc.items]))
 
-                        elif key == 'REQ_TEXT':
-                            item.text = field
-                        elif key == 'REQ_RATIO':
-                            item.set(name='rationale', value=field)
-                        elif key == 'REQ_NOTE':
-                            item.set(name='note', value=field)
+            elif text == 'REQ_NUM':
+                for key in ['REQ_NUM', 'REQ_TEXT', 'REQ_RATIO', 'REQ_NOTE']:
+                    field = _read_next_and_forward(paragraph)
 
-            except StopIteration:
-                break
+                    if key == 'REQ_NUM':
+                        num = int(field)
+                        if num not in reqs:
+                            # create
+                            item = doc.add_item(num)
+                            print('new requirement')
+                        else:
+                            # update
+                            uid = prefix + '{:0>3d}'.format(num)
+                            item = doc.find_item(uid)
+                            print('update requirement')
+                        input_reqs.append(num)
 
+                    elif key == 'REQ_TEXT':
+                        item.text = field
+                    elif key == 'REQ_RATIO':
+                        item.set(name='rationale', value=field)
+                    elif key == 'REQ_NOTE':
+                        item.set(name='note', value=field)
+
+        except StopIteration:
+            break
+
+    if reqs:
         for key in (set(reqs) - set(input_reqs)):
             # delete
             uid = prefix + '{:0>3d}'.format(key)
@@ -88,3 +101,27 @@ class Document(object):
             print('delete requirement')
 
         print(doc.items)
+
+
+repopath = './examples/reqs' # TODO
+tree = doorstop.build(root=repopath)
+print('On tree {}'.format(repopath))
+print(tree)
+
+while True:
+    try:
+        docpath = input('Add document: ')
+        document = zipfile.ZipFile(docpath)
+        xml_content = document.read('word/document.xml')
+        document.close()
+        doctree = XML(xml_content)
+        process_document(repopath, tree, doctree, _create)
+        more = str(input('Add more? '))
+        if more == 'n':
+            break
+        else:
+            continue
+
+    except FileNotFoundError:
+        print('File not found!')
+        continue
